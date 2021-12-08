52e27d86ec4fa5fa4b26686da5d8ec710ca6a44b
Deflaktor <deflaktor@gmail.com>
2021-12-08 12:15:15 +0100

    Update batchConvert.py to create bsdiff files and sha1.yaml

--- eventSquare/batchConvert.py
+++ eventSquare/batchConvert.py
@@ -8,14 +8,11 @@ from subprocess import check_output
 from pathlib import Path
 
 import os
-import json
-import urllib.request
-import csv
-import yaml
-import gdown
 import shutil
 from PIL import Image
 import imagehash
+import hashlib
+import yaml
 from multiprocessing import Process
 
 def deleteFolderContents(folder : str):
@@ -27,6 +24,19 @@ def deleteFolderContents(folder : str):
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+def get_digest(file_path):
+    h = hashlib.sha1()
+
+    with open(file_path, 'rb') as file:
+        while True:
+            # Reading is buffered, so we can read smaller chunks.
+            chunk = file.read(h.block_size)
+            if not chunk:
+                break
+            h.update(chunk)
+
+    return h.hexdigest()
+
 def process(lang : str):
     langDir = f'lang{lang}/'
     suffix = ""
@@ -37,27 +47,39 @@ def process(lang : str):
 
     filePaths = list(Path().glob(f'chance_card/{langDir}*.cmpres'))
     gameBoardCmpresPath = list(Path().glob(f'game_board/game_board{suffix}.cmpres'))[0]
-
     gameBoardOutputDir = f'output/game/{langDir}'
     gameBoardOutput = gameBoardOutputDir + f'game_board{suffix}.cmpres'
     imageOutputDir = f'output/images/{langDir}'
     imageOutput = imageOutputDir + "ui_mark_eventsquare_{number}.png"
     chanceCardOutputDir = f'output/chance_card/{langDir}'
+    chanceCardBsdiffDir = f'output/bsdiff/chance_card/{langDir}'
+    gameBoardBsdiffDir = f'output/bsdiff/game/{langDir}'
+    gameBoardBsdiff = gameBoardBsdiffDir + f'game_board{suffix}.cmpres.bsdiff'
 
     if os.path.exists(imageOutputDir):
         deleteFolderContents(imageOutputDir)
     else:
-        os.mkdir(imageOutputDir)
+        os.makedirs(imageOutputDir)
 
     if os.path.exists(chanceCardOutputDir):
         deleteFolderContents(chanceCardOutputDir)
     else:
-        os.mkdir(chanceCardOutputDir)
+        os.makedirs(chanceCardOutputDir)
 
     if os.path.exists(gameBoardOutputDir):
         deleteFolderContents(gameBoardOutputDir)
     else:
-        os.mkdir(gameBoardOutputDir)
+        os.makedirs(gameBoardOutputDir)
+
+    if os.path.exists(chanceCardBsdiffDir):
+        deleteFolderContents(chanceCardBsdiffDir)
+    else:
+        os.makedirs(chanceCardBsdiffDir)
+
+    if os.path.exists(gameBoardBsdiffDir):
+        deleteFolderContents(gameBoardBsdiffDir)
+    else:
+        os.makedirs(gameBoardBsdiffDir)
 
     gameBoardCmpres = gameBoardCmpresPath.as_posix()
     gameBoardCmpresDecomp = f'{gameBoardCmpresPath.parent.as_posix()}/{gameBoardCmpresPath.stem}_DECOMP.bin'
@@ -91,9 +113,11 @@ def process(lang : str):
             print(check_output(f'wszst CREATE {fileExtractDir} --overwrite --dest {fileDecomp}', encoding="utf-8"))
             fileOutput = chanceCardOutputDir + f'{filePath.stem}.cmpres'
             print(check_output(f'ntcompress -lex {fileDecomp} -o {fileOutput}', encoding="utf-8"))
+            fileBsdiff = chanceCardBsdiffDir + f'{filePath.stem}.cmpres.bsdiff'
+            print(check_output(f'bsdiff {file} {fileOutput} {fileBsdiff}', encoding="utf-8"))
 
-        print(check_output(f'magick MOGRIFY -crop 170x170+5+35 -resize 29x32 -alpha off {chanceCardPngFile}', encoding="utf-8")) # for ui_mark
-        #print(check_output(f'magick MOGRIFY +level 0%,50% -crop 170x170+5+35 -resize 128x128 -alpha off {chanceCardPngFile}', encoding="utf-8"))
+        #print(check_output(f'magick MOGRIFY -crop 170x170+5+35 -resize 29x32 -alpha off {chanceCardPngFile}', encoding="utf-8")) # for ui_mark
+        print(check_output(f'magick MOGRIFY +level 0%,50% -crop 170x170+5+35 -resize 128x128 -alpha off {chanceCardPngFile}', encoding="utf-8")) # for game_board.cmpres
         #print(check_output(f'magick MOGRIFY -crop 170x170+5+35 -resize 64x64 -alpha off {chanceCardPngFile}', encoding="utf-8"))
         #print(check_output(f'magick MOGRIFY -crop 170x170+5+35 -resize 128x128 -alpha off {chanceCardPngFile}', encoding="utf-8"))
 
@@ -110,11 +134,15 @@ def process(lang : str):
 
     print(check_output(f'wszst CREATE {gameBoardExtractDir} --overwrite --dest {gameBoardCmpresDecomp}', encoding="utf-8"))
     print(check_output(f'ntcompress -lex {gameBoardCmpresDecomp} -o {gameBoardOutput}', encoding="utf-8"))
+    print(check_output(f'bsdiff {gameBoardCmpres} {gameBoardOutput} {gameBoardBsdiff}', encoding="utf-8"))
+    shutil.rmtree(gameBoardExtractDir)
     os.remove(gameBoardCmpresDecomp)
 
     filePaths = list(Path().glob(imageOutputDir + "*.png"))
 
     hashToFiles = {}
+
+    
 
     for filePath in filePaths:
         hash = imagehash.average_hash(Image.open(filePath), hash_size=20)
@@ -125,7 +153,7 @@ def process(lang : str):
 
     for hash in hashToFiles:
         if len(hashToFiles[hash]) > 1:
-            print("SHA1: {0}".format(hash))
+            print("Hash: {0}".format(hash))
             for file in hashToFiles[hash]:
                 print("      {0}".format(file))
 
@@ -140,6 +168,25 @@ def main(argv : list):
     if not os.path.exists('output/chance_card'):
         os.mkdir('output/chance_card')
 
+    with open('output/sha1.yaml', 'w', encoding = "utf-8") as yaml_file:
+        fileToSha1 = {}
+        lang = ""
+        for lang in ["", "DE", "EN", "ES", "FR", "IT", "UK"]:
+            langDir = f'lang{lang}/'
+            suffix = ""
+            if lang:
+                suffix = f'_{lang}'
+            else:
+                langDir = ''
+            filePaths = list(Path().glob(f'chance_card/{langDir}*.cmpres'))
+            gameBoardCmpresPath = list(Path().glob(f'game_board/game_board{suffix}.cmpres'))[0]
+            fileToSha1[gameBoardCmpresPath.as_posix()] = get_digest(gameBoardCmpresPath.as_posix())
+            for filePath in filePaths:
+                file = filePath.as_posix()
+                fileToSha1[file] = get_digest(file)
+        dump = yaml.dump(fileToSha1, default_flow_style = False, allow_unicode = True, encoding = None)
+        yaml_file.write( dump )
+
     processes = []
 
     lang = ""
@@ -150,7 +197,6 @@ def main(argv : list):
     
     for p in processes:
         p.join()
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
