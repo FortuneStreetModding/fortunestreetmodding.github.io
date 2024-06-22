71c798e5004f7dc076a7e204fc9fbae539023174
Deflaktor <deflaktor@gmail.com>
2024-06-23 01:43:25 +0200

    Add github action for deploying astro page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fortunestreetmodding.github.io/',
   integrations: [solidJs()],
   vite: {
     plugins: [
