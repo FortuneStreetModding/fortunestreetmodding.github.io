5d1e40f6d1773b9872df78e2e29860fb01bd86a8
Deflaktor <deflaktor@gmail.com>
2024-06-23 20:35:57 +0200

    Move css into respective components

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import yaml from '@rollup/plugin-yaml';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fortunestreetmodding.github.io/',
+  build: {
+    format: 'file'
+  },
   vite: {
     plugins: [
       yaml()
