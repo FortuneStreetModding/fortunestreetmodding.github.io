8fd6fba73f60b2a6d975a6d6275a0d7f5f8b162c
Deflaktor <deflaktor@gmail.com>
2024-06-25 00:02:55 +0200

    Fix boards.json getting wrongly generated

--- src/pages/boards.json.ts
+++ src/pages/boards.json.ts
@@ -3,9 +3,8 @@ import boards from "~/lib/getboards";
 // We want to omit some properties to reduce the size of the json transferred
 function omitPropertiesFromArray<T>(arr: T[], propertiesToOmit: (keyof T)[]): string {
     return JSON.stringify(arr, function(key, value) {
-        // The top-level properties of the objects will have an empty string as the parent key
-        if (this === arr || this instanceof Array) {
-            // If the key is in the list of properties to omit, return undefined to omit it
+        // check if we are on top-level
+        if (Object.keys(this).includes("frbFiles") && Object.keys(this).includes("targetAmount")) {
             if (propertiesToOmit.includes(key as keyof T)) {
                 return undefined;
             }
